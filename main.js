5a8bc8c8b89f36605827708146c02bd6d9ac49a6
System Administrator <root@xuexuedeMacBook-Air.local>
2017-12-31 04:32:24 -0800

    vul

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var server = require('./server');
+var ip = require('ip');
+console.log(ip.address())
 server({
     startLocalPort:10000,
-    localIp:'0.0.0.0',
+    localIp:ip.address(),
     port:3000
 })
