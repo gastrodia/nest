98d3b0ffdfedaa89e6f23d4b722485aa2ceacb7c
System Administrator <root@xuexuedeMacBook-Air.local>
2018-01-01 18:57:28 +0800

    jboss:as-6.1.0

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,4 @@
 
 docker build --rm -f apps/node/Dockerfile -t gastrodia/node:latest apps/node
 docker build --rm -f apps/jdk/Dockerfile -t gastrodia/jdk:latest apps/jdk
+docker build --rm -f apps/jboss/as-6.1.0/Dockerfile -t gastrodia/jboss:as-6.1.0 apps/jboss/as-6.1.0
