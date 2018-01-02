18f289e919e0979ea9c5b67afd5f31da27bc605a
gastrodia <nil@sanwu.org>
2018-01-02 08:48:03 +0800

    jdk

--- init-dev.sh
+++ init-dev.sh
@@ -4,4 +4,4 @@ wget https://npm.taobao.org/mirrors/node/v8.9.3/node-v8.9.3-linux-x64.tar.gz && 
     rm node-v8.9.3-linux-x64.tar.gz 
 cp daemon.json /etc/docker/daemon.json; 
 service docker restart;
-npm run dev-server & npm run dev-client;
+#npm run dev-server & npm run dev-client;
