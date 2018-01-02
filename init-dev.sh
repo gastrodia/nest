805cebbf9536c16a54c6f8d08ede966eab0444fb
gastrodia <gastrodia@outlook.com>
2018-01-02 13:24:17 +0800

    remove graph

--- init-dev.sh
+++ init-dev.sh
@@ -1,7 +1,7 @@
 apt install docker.io docker-compose wget -y
-wget https://npm.taobao.org/mirrors/node/v8.9.3/node-v8.9.3-linux-x64.tar.gz && \
-    tar -C /usr/local --strip-components 1 -xzf node-v8.9.3-linux-x64.tar.gz && \
-    rm node-v8.9.3-linux-x64.tar.gz 
 cp daemon.json /etc/docker/daemon.json; 
 service docker restart;
+# wget https://npm.taobao.org/mirrors/node/v8.9.3/node-v8.9.3-linux-x64.tar.gz && \
+#     tar -C /usr/local --strip-components 1 -xzf node-v8.9.3-linux-x64.tar.gz && \
+#     rm node-v8.9.3-linux-x64.tar.gz 
 #npm run dev-server & npm run dev-client;
