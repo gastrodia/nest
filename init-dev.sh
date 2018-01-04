3f549fdb6c807a97010ab2d5aaa02a9ba33ca936
木浦 <gastrodia@users.noreply.github.com>
2018-01-04 08:15:38 +0000

    Update init-dev.sh

--- init-dev.sh
+++ init-dev.sh
@@ -1,4 +1,9 @@
-apt install docker.io docker-compose wget -y
+apt-get install apt-transport-https 
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9 
+bash -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list" 
+apt-get update 
+apt-get install lxc-docker
+apt install docker-compose wget -y
 cp daemon.json /etc/docker/daemon.json; 
 service docker restart;
 # wget https://npm.taobao.org/mirrors/node/v8.9.3/node-v8.9.3-linux-x64.tar.gz && \
