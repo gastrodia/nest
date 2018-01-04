68849cf6fe711e43b1e5a7b308db03ce476b9bb2
木浦 <gastrodia@users.noreply.github.com>
2018-01-04 16:51:35 +0800

    Update init-dev.sh

--- init-dev.sh
+++ init-dev.sh
@@ -1,11 +1,26 @@
-apt-get install apt-transport-https 
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9 
-bash -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list" 
-apt-get update 
-apt-get install lxc-docker
-apt install docker-compose wget -y
-cp daemon.json /etc/docker/daemon.json; 
-service docker restart;
+
+
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+ sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update 
+sudo apt-get install docker-ce
+
+sudo apt install docker-compose wget -y
+sudo cp daemon.json /etc/docker/daemon.json; 
+sudo service docker restart;
 # wget https://npm.taobao.org/mirrors/node/v8.9.3/node-v8.9.3-linux-x64.tar.gz && \
 #     tar -C /usr/local --strip-components 1 -xzf node-v8.9.3-linux-x64.tar.gz && \
 #     rm node-v8.9.3-linux-x64.tar.gz 
