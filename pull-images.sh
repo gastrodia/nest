b3e9d515cc3d607601629d93a251b424f5b6b538
root <root@localhost.localdomain>
2018-01-06 19:46:44 +0800

    abc

--- pull-images.sh
+++ pull-images.sh
@@ -1,3 +1,6 @@
+docker pull ubuntu:16.04
 docker pull gastrodia/node
 docker pull gastrodia/jdk
 docker pull gastrodia/jboss:as-6.1.0 
+docker pull gastrodia/jexboss
+docker pull gastrodia/discourse
